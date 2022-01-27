29af3a70fd23436f0b0528cbafb669c2ceb1fe08
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2022-01-27 03:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-let posX, posY, mouseDown;
+let brush = {
+    el: $("#brush"),
+    size: $("#sizePicker").val(),
+    colour: $("#colourPicker").val(),
+    posX: 0,
+    posY: 0,
+    paint: false,
+    followCursor: function () {
+        this.el.css({
+            "left": this.posX, "top": this.posY,
+            "height": this.size, "width": this.size
+        });
+    },
+    placeSplotch: function () {
+        // Put a dot where the user clicks.
+        $("#canvas").append('<div class="splotch" style="position:absolute; left:'
+            + this.posX + 'px; top:' + this.posY + 'px; height:' + this.size
+            + 'px; width:' + this.size + 'px; background-color:'
+            + this.colour + ';"></div>');
+    }
+};
 
-// Get mouse position with respect to the canvas
-$("#canvas").mousemove((e) => {
-    posX = e.pageX - $("#sizePicker").val() / 2 // Centre the brush by subtracting half of its width from the x position
-    posY = e.pageY - $("#sizePicker").val() / 2; // and half of its height from the y position
-});
-
-const followCursor = () => {
-    // Make the brush element follow the cursor position
-    $("#brush").css("left", posX);
-    $("#brush").css("top", posY);
-    // Change the brush size as selected by the user
-    $("#brush").css("height", $("#sizePicker").val());
-    $("#brush").css("width", $("#sizePicker").val());
-}
-
-// Run followCursor() while the cursor is on the canvas
-$(document).mousemove(() => {
-    if ($("#canvas:hover").length != 0) followCursor();
-    if (mouseDown) placeSplotch();
+$(document).mousemove((e) => {
+    brush.posX = e.pageX - brush.size / 2; // Get the centre the brush by subtracting half of its width from the x position
+    brush.posY = e.pageY - brush.size / 2; // and half of its height from the y position.
+    if (brush.paint) brush.placeSplotch(); // Place splotches if the user is holding the mouse down.
+    if ($("#canvas:hover").length != 0) brush.followCursor(); // Run brush.followCursor() while the mouse is over the canvas.
 });
 
 $("#canvas").mousedown(() => {
-    placeSplotch();
-    mouseDown = true
+    brush.placeSplotch(); // 1 splotch placed if the user clicks and doesn't move the mouse
+    brush.paint = true;
 });
-$("#canvas").mouseup(() => mouseDown = false);
-
-// Put a dot where the user clicks
-const placeSplotch = () => $("#canvas").append('<div class="splotch" style="position:absolute; left:'
-    + posX + 'px; top:' + posY + 'px; height:' + $("#sizePicker").val()
-    + 'px; width:' + $("#sizePicker").val() + 'px; background-color:'
-    + $("#colourPicker").val() + ';"></div>');
+$("#canvas").mouseup(() => brush.paint = false);
